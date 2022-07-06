dbf24ae5000b1ae978a540c79132bfbdf260c21c
hayden.chae <hayden.chae@kakaocorp.com>
2022-07-06 16:14:51 +0900

    LineView 구현

---
+++ swift-drawingapp/View/DrawingView.swift
@@ -0,0 +1,20 @@
+//
+//  DrawingView.swift
+//  swift-drawingapp
+//
+//  Created by kakao on 2022/07/06.
+//
+
+import UIKit
+
+class DrawingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ swift-drawingapp/View/LineView.swift
@@ -0,0 +1,20 @@
+//
+//  LineView.swift
+//  swift-drawingapp
+//
+//  Created by kakao on 2022/07/06.
+//
+
+import UIKit
+
+class LineView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
